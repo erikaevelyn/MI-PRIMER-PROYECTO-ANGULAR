1b3e38e1fe9e1353360b89797c06c3a708889b75
Erika Rodriguez <erikaevelynrodriguez@gmail.com>
2019-07-29 22:05:22 -0300

    Avances

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MI-PRIMER-PROYECTO';
+  nombre = 'Erika';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductoComponent } from './producto/producto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductoComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, ProductoComponent] 
 })
 export class AppModule { }
